dd4dcb41d1812551bfb384d452f27161d58e24e4
luminous <luminous123@163.com>
2016-07-26 11:21:07 +0800

    first commit

---
+++ src/main/java/stock/controller/HomeController.java
@@ -0,0 +1,20 @@
+package stock.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+
+    @RequestMapping({"/","/index"})
+    public String showHomePage(Map<String,Object> model)
+    {
+        model.put("WelcomeWords", "»¶Ó­Äã£¬ÀÏ´ó");
+        //System.out.println("showHomepage executed, using stock");
+        
+        return "index";
+    }
+}
